048e17ff3f32676af47e55550e7507b492e65cb0
HollTil <detilman83@gmail.com>
2024-01-26 10:12:54 +0100

    set activity diagram

--- src/main/java/de/srh/holtil/Main.java
+++ src/main/java/de/srh/holtil/Main.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
+        Account account1 = new Account("Tilles", 123446543, 2345, 500.00 , active);
+
     }
 }
